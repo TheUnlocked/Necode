6c520c3183fdbeb8551f512ad0b4813222183405
TheUnlocked <10186337+TheUnlocked@users.noreply.github.com>
2022-12-05 15:03:33 -0500

    Update y-utils.ts

--- src/util/y-utils.ts
+++ src/util/y-utils.ts
@@ -1,9 +1,15 @@
 import * as Y from 'yjs';
 
-export function applyUnifiedUpdates(doc: Y.Doc, update: (doc: Y.Doc) => void, unifiedChannel = 0) {
+/**
+ * Use this to apply updates to a yjs document that every member of the network can perform while avoiding duplication.
+ * This is often used for initializing the document state
+ * @param doc The document to update
+ * @param update A callback. Changes to the document in this update will be reflected across the entire network one time
+ */
+export function applyUnifiedUpdates(doc: Y.Doc, update: (doc: Y.Doc) => void) {
     // Initialize "default content" doc
     const unifiedDoc = new Y.Doc();
-    unifiedDoc.clientID = unifiedChannel;
+    unifiedDoc.clientID = 0;
 
     // Generate default content state
     update(unifiedDoc);
